b832e50fbe9820e3efed3e49362affed98bdd091
Rishit Das <rdas@ucdavis.edu>
2023-12-03 16:15:46 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Your JS code here
+  window.addEventListener('scroll', () => {
+    // We will add a class to the character icon when a section is in view
+  });
+});
